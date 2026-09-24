fa3afa2cfa48427969a9e92368baf5c108b856f9
Sumit kumar <sumit.ydv.kumar2006@gmail.com>
2026-09-24 20:17:53 +0530

    basic crud logic done for cities

--- src/service/city-service.js
+++ src/service/city-service.js
@@ -32,7 +32,7 @@ class CityService{
             throw {error}
         }
     }
-    async updateCity(data,cityId){
+    async updateCity(cityId,data){
         try {
             const city=await this.cityRepository.updateCity(cityId,data);
             return city;
